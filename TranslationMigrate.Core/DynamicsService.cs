d6d6adbc6de6560fcb60696bfceaad68f9638465
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-09 20:24:41 +0300

    org service abstracted

--- TranslationMigrate.Core/DynamicsService.cs
+++ TranslationMigrate.Core/DynamicsService.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.Xrm.Sdk.Client;
+﻿using System;
+using Microsoft.Xrm.Sdk.Client;
+using System.Configuration;
+using System.ServiceModel.Description;
 
 namespace TranslationMigrate.Core
 {
@@ -8,9 +11,32 @@ namespace TranslationMigrate.Core
 
         public DynamicsService(OrganizationServiceProxy organizationServiceProxy)
         {
-            _organizationServiceProxy = organizationServiceProxy;
+            _organizationServiceProxy = LoadOrganizationService();
         }
 
+        private OrganizationServiceProxy LoadOrganizationService()
+        {
+            var userName = ConfigurationSettings.AppSettings["CRMUserName"].ToString();
+            var password = ConfigurationSettings.AppSettings["CRMUserPassword"].ToString();
+            var serviceUrl = $"{ConfigurationSettings.AppSettings["CRMOrgURL"].ToString()}/XRMServices/2011/Organization.svc";
 
+            OrganizationServiceProxy service = null;
+            try
+            {
+                ClientCredentials credentials = new ClientCredentials();
+                credentials.UserName.UserName = userName;
+                credentials.UserName.Password = password;
+                Uri serviceUri = new Uri(serviceUrl);
+                service = new OrganizationServiceProxy(serviceUri, null, credentials, null);
+                service.EnableProxyTypes();
+
+            }
+            catch (Exception exception)
+            {
+                return null;
+                //Log the Error message
+            }            
+            return service;
+        }
     }
 }
