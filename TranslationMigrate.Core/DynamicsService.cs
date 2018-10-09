8105e78226aa42d5b8b68f6273b1c26279e43d76
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-09 19:28:47 +0300

    org service added

---
+++ TranslationMigrate.Core/DynamicsService.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Xrm.Sdk.Client;
+
+namespace TranslationMigrate.Core
+{
+    internal class DynamicsService : IDynamicsService
+    {
+        private readonly OrganizationServiceProxy _organizationServiceProxy;
+
+        public DynamicsService(OrganizationServiceProxy organizationServiceProxy)
+        {
+            _organizationServiceProxy = organizationServiceProxy;
+        }
+
+
+    }
+}
