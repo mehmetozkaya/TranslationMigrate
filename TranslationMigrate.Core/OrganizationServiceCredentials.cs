1af5fe870985d5acb23407d5cd5b98096104c12a
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-12 21:18:55 +0300

    OrganizationServiceCredentials developing..

--- TranslationMigrate.Core/OrganizationServiceCredentials.cs
+++ TranslationMigrate.Core/OrganizationServiceCredentials.cs
@@ -6,5 +6,15 @@
         public string Password { get; set; }
         public string Url { get; set; }
 
+
+
+        //var userName = ConfigurationSettings.AppSettings["CRMUserName"].ToString();
+        //var password = ConfigurationSettings.AppSettings["CRMUserPassword"].ToString();
+        //var serviceUrl = $"{ConfigurationSettings.AppSettings["CRMOrgURL"].ToString()}/XRMServices/2011/Organization.svc";
+
+
+        private const string DevUserNameConfiguration = "DevUserName";
+        private const string DevPasswordConfiguration = "DevPassword";
+        private const string DevURLConfiguration = "DevURL";
     }
 }
