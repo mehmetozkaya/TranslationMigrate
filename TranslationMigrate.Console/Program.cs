9f78426d01e57072f9ad03213d6a13f6b317e67e
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-12 12:20:17 +0300

    OrganizationServiceCredentials

--- TranslationMigrate.Console/Program.cs
+++ TranslationMigrate.Console/Program.cs
@@ -12,6 +12,8 @@ namespace TranslationMigrate.Console
         static void Main(string[] args)
         {
             IDynamicsService dynamicsService = new DynamicsService();
+
+
         }
     }
 }
---
+++ TranslationMigrate.Core/OrganizationServiceCredentials.cs
@@ -0,0 +1,10 @@
+﻿namespace TranslationMigrate.Core
+{
+    internal class OrganizationServiceCredentials
+    {
+        public string UserName { get; set; }
+        public string Password { get; set; }
+        public string Url { get; set; }
+
+    }
+}
