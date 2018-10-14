e03ceb24b4b74ef624c607f1faa3c5959359809c
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-14 15:26:29 +0300

    comment desc

--- TranslationMigrate.Console/Program.cs
+++ TranslationMigrate.Console/Program.cs
@@ -7,6 +7,12 @@ using TranslationMigrate.Core;
 
 namespace TranslationMigrate.Console
 {
+    /// <summary>
+    /// Fetch last 5000 record from MasterDev
+    /// Get last 10 day record which last modified on from Dev
+    /// Compare Dev to MasterDev, if masterDev has no record, directly insert MasterDev db. 
+    /// If exist and not match directly update.
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
