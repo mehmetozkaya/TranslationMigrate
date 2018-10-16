b749cfc0cb61228bbea3efefe1167bbb7f58bc2d
Mehmet Ozkaya <mehmetozkaya@gmail.com>
2018-10-16 23:04:10 +0300

    IComparable

--- TranslationMigrate.Core/TranslationManager.cs
+++ TranslationMigrate.Core/TranslationManager.cs
@@ -4,7 +4,7 @@ using System.Collections.Generic;
 
 namespace TranslationMigrate.Core
 {
-    public class TranslationManager : ITranslationManager
+    public class TranslationManager : ITranslationManager, IComparable
     {
         public TranslationManager()
         {            
@@ -115,6 +115,11 @@ namespace TranslationMigrate.Core
                     TranslationSpanish = entityCollectionSpanish
                 };
             };
+        }
+
+        public int CompareTo(object obj)
+        {
+            throw new NotImplementedException();
         }
     }
 }
